c7258206410e4a4dfc85a3d82733b326fa80d705
Soma Somorjai <somafet@gmail.com>
2025-07-03 09:54:48 +0200

    Adjust posthog endpoint in next-demo.

--- apps/examples/nextjs-demo/components/posthog/PostHogProvider.tsx
+++ apps/examples/nextjs-demo/components/posthog/PostHogProvider.tsx
@@ -9,7 +9,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     if (!process.env.NEXT_PUBLIC_DISABLE_POSTHOG) {
       posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: "/ingest",
+        api_host: "/buda",
         ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
         capture_pageview: false, // We capture pageviews manually
         capture_pageleave: true, // Enable pageleave capture
--- apps/examples/nextjs-demo/next.config.ts
+++ apps/examples/nextjs-demo/next.config.ts
@@ -4,15 +4,15 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/ingest/static/:path*",
+        source: "/buda/static/:path*",
         destination: "https://eu-assets.i.posthog.com/static/:path*",
       },
       {
-        source: "/ingest/:path*",
+        source: "/buda/:path*",
         destination: "https://eu.i.posthog.com/:path*",
       },
       {
-        source: "/ingest/decide",
+        source: "/buda/decide",
         destination: "https://eu.i.posthog.com/decide",
       },
     ];
